e96384896f871b668a856d41a4eb144478f2adc8
Liam Davies <liam@Liams-MacBook-Air.local>
2025-08-26 21:22:02 +1000

    100 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,15 +7,33 @@ public class PrintAndHide : MonoBehaviour
     private int i;
     // Start is called before the first frame update
     public Renderer rend;
+    private int blueDisableFrame;
     void Start()
     {
         i = 3;
+        if (CompareTag("Blue"))
+        {
+            blueDisableFrame = Random.Range(150, 251);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log(gameObject.name + " : " + i);
+        Debug.Log(gameObject.name + ":" + i);
+        if (CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        // If BluePrefab hits its random number → disable Renderer
+        if (CompareTag("Blue") && i == blueDisableFrame)
+        {
+            if (rend != null)
+            {
+                rend.enabled = false;
+            }
+        }
     }
 }
